b0d3757a5187f53fe529abd9464dca2472939de9
daxgdev <faniel5431@gmail.com>
2024-10-15 17:01:06 -0500

    feat(main): añadir interacción y funcionalidad al formulario de contacto

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,71 @@ document.addEventListener('click', (e) => {
     $dropdownButton.classList.toggle('active')
   }
 })
+
+// Manejo del formulario de contacto
+const $form = $('.contact-form')
+const $formButton = $('.contact-form > .send-form')
+let messageContent = []
+
+const validate = (type) => {
+  const $input = document.querySelector(`#${type}`)
+  const $error = $input.nextElementSibling
+  let value = $input.value
+  value = value.trim()
+
+  // Validaciones de los campos
+  if (!value) {
+    $error.innerHTML = `<img src="assets/xmark-solid.svg" alt="Ícono de X" /> Debes ingresar un ${type}`
+    if (!$error.classList.contains('active')) $error.classList.add('active')
+    if (!$input.classList.contains('error')) $input.classList.add('error')
+  } else {
+    if ($error.classList.contains('active')) $error.classList.remove('active')
+    if ($input.classList.contains('error')) $input.classList.remove('error')
+    messageContent.push([type, value])
+  }
+}
+
+$form.onsubmit = (e) => {
+  e.preventDefault()
+  messageContent = []
+
+  validate('nombre')
+  validate('correo')
+  validate('asunto')
+  validate('mensaje')
+
+  if (messageContent.length === 4) {
+    $formButton.setAttribute('disabled', '')
+    $formButton.textContent = 'Enviando...'
+    messageContent = Object.fromEntries(messageContent)
+
+    fetch('https://formsubmit.co/ajax/faniel5431@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(messageContent)
+    })
+      .then(() => {
+        $formButton.textContent = 'Enviado'
+        $formButton.classList.add('success')
+
+        setTimeout(() => {
+          $formButton.removeAttribute('disabled')
+          $formButton.textContent = 'Enviar mensaje'
+          $formButton.classList.remove('success')
+        }, 2500)
+      })
+      .catch(() => {
+        $formButton.textContent = 'Ocurrió un error'
+        $formButton.classList.add('error')
+
+        setTimeout(() => {
+          $formButton.removeAttribute('disabled')
+          $formButton.textContent = 'Enviar mensaje'
+          $formButton.classList.remove('error')
+        }, 2500)
+      })
+  }
+}
